5c5109260e8fee3f12532fc2c150583a3a924960
kai <kai@cloudpack.jp>
2018-08-22 14:49:52 +0900

    DockerでVue.jsをTypeScript+SCSS+Bootstrap+Material Designで開発できる環境を構築した。

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue_js_typescript_bootstrap_material
+
+## Description
+
+## Usage
+
+Dockerを利用する場合
+
+```sh
+> cd vue_js_typescript_bootstrap_material
+> docker-compose up -d
+> docker-compose exec app bash
+
+>> cd app
+>> yarn
+>> yarn serve
+```
+
+```sh
+> http://localhost:8080/
+```
---
+++ app/src/views/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <btn color="primary">Primary</btn>
+  </div>
+</template>
+
+<script lang="ts">
+  import {
+    Component,
+    Vue
+  } from 'vue-property-decorator';
+  
+  import {
+    Container,
+    Navbar,
+    NavbarItem,
+    NavbarNav,
+    NavbarCollapse,
+    Btn,
+    Fa,
+    ListGroup,
+    ListGroupItem,
+    CardBody,
+    Footer,
+    waves,
+  } from 'mdbvue';
+  @Component({
+    components: {
+      Container,
+      Navbar,
+      NavbarItem,
+      NavbarNav,
+      NavbarCollapse,
+      Btn,
+      Fa,
+      ListGroup,
+      ListGroupItem,
+      CardBody,
+      Footer,
+      waves,
+    },
+  })
+  export default class About extends Vue {}
+</script>
